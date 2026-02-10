25de45dab57859f436c8dabf82ede11b506c9f33
OakLasagna <gabor200503@gmail.com>
2026-02-10 15:08:37 +0100

    Hűségprogram oldal

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,10 @@ router.get('/helyfoglalas', (request, response) => {
     response.sendFile(path.join(__dirname, '../frontend/html/helyfoglalas.html'));
 });
 
+router.get('/husegprogram', (request, response) => {
+    response.sendFile(path.join(__dirname, '../frontend/html/husegprogram.html'));
+});
+
 //!API endpoints
 app.use('/', router);
 const endpoints = require('./api/api.js');
