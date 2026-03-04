69ffa5d4a1b514107ac39765167bb241fe9f9748
meszgergo <119933758+meszgergo@users.noreply.github.com>
2026-03-04 23:33:58 +0100

    Működő helyfoglalás
    A felhasználó innentől tud foglalni ha végig nyomja a keresési folyamatot majd üléseket választ és a foglalás gombra nyom.

--- backend/api/api.js
+++ backend/api/api.js
@@ -41,4 +41,474 @@ router.get('/testsql', async (request, response) => {
     }
 });
 
+<<<<<<< Updated upstream
+=======
+router.get('/AdminCheck', async (request, response) => {
+    try {
+        if (!LoggedInCheck(request)) {
+            return response.status(220).json({
+                admin: false
+            })
+        }
+        else {
+            response.status(200).json({
+                admin: request.session.user.role == 1
+            })
+        }
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/availabledepartureairports', async (request, response) => {
+    try {
+        const result = await database.selectAvailableDepartureAirports();
+
+        let airportcodes = [];
+        for (let i = 0; i < result.length; i++) {
+            airportcodes.push(result[i].DepartureAirport);
+        }
+
+        response.status(200).json({
+            airportcodes: airportcodes
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/availablearrivalairports', async (request, response) => {
+    try {
+        const result = await database.selectAvailableArrivalAirports();
+
+        let airportcodes = [];
+        for (let i = 0; i < result.length; i++) {
+            airportcodes.push(result[i].ArrivalAirport);
+        }
+
+        response.status(200).json({
+            airportcodes: airportcodes
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/availableflights', async (request, response) => {
+    try {
+        const result = await database.selectAvailableFlightsBasedOnParameters(((request.query.departureAirport == undefined) ? "" : request.query.departureAirport), ((request.query.arrivalAirport == undefined) ? "" : request.query.arrivalAirport), ((request.query.departureDate == undefined) ? "" : request.query.departureDate));
+
+
+        response.status(200).json({
+            result: result
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/availabledepartureairportsfiltered', async (request, response) => {
+    try {
+        const result = await database.selectAvailableDepartureAirportsFilteredHun(((request.query.arrivalAirport == undefined) ? "" : request.query.arrivalAirport), ((request.query.departureDate == undefined) ? "" : request.query.departureDate));
+
+
+        response.status(200).json({
+            results: result
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/availablearrivalairportsfiltered', async (request, response) => {
+    try {
+        const result = await database.selectAvailableArrivalAirportsFilteredHun(((request.query.departureAirport == undefined) ? "" : request.query.departureAirport), ((request.query.departureDate == undefined) ? "" : request.query.departureDate));
+
+        response.status(200).json({
+            results: result
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/availabledeparturedatesfiltered', async (request, response) => {
+    try {
+
+        const result = await database.selectAvailableDepartureDatesFiltered(((request.query.departureAirport == undefined) ? "" : request.query.departureAirport), ((request.query.arrivalAirport == undefined) ? "" : request.query.arrivalAirport));
+
+        let departuredates = [];
+        for (let i = 0; i < result.length; i++) {
+            departuredates.push(result[i].DepartureDate);
+        }
+
+        response.status(200).json({
+            departuredates: departuredates
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/availablearrivaldatesfiltered', async (request, response) => {
+    try {
+
+        const result = await database.selectAvailableArrivalDatesFiltered(request.query.departureAirport, request.query.arrivalAirport, request.query.departureDate);
+
+        let arrivaldates = [];
+        for (let i = 0; i < result.length; i++) {
+            arrivaldates.push(result[i].ArrivalDate);
+        }
+
+        response.status(200).json({
+            arrivaldates: arrivaldates
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/availablereturndates', async (request, response) => {
+    try {
+
+        const result = await database.selectAvailableReturnDates(request.query.departureAirport, request.query.arrivalAirport, request.query.destinationArrivalDate);
+
+        let returndates = [];
+
+        for (let i = 0; i < result.length; i++) {
+            returndates.push(result[i].ReturnDate);
+        }
+
+        response.status(200).json({
+            returndates: returndates
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/swappableairportswithsamedeparturedates', async (request, response) => {
+    try {
+
+        const result = await database.selectSwappableFlightsWithSameDepartureDates(((request.query.departureDate == undefined) ? "" : request.query.departureDate));
+
+        let airports = [];
+        for (let i = 0; i < result.length; i++) {
+            airports.push(result[i].DepartureAirport)
+
+        }
+
+        response.status(200).json({
+            airports: airports
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/swappableairports', async (request, response) => {
+    try {
+
+        const result = await database.selectSwappableFlights();
+
+        let airports = [];
+        for (let i = 0; i < result.length; i++) {
+            airports.push(result[i].DepartureAirport)
+
+        }
+
+        response.status(200).json({
+            airports: airports
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/flights', async (request, response) => {
+    try {
+
+        if (request.query.departureAirport == undefined || request.query.arrivalAirport == undefined || request.query.departureDate == undefined || request.query.numOfPassengers == undefined) {
+            response.status(400).json({
+                error: request.t("errors", {returnObjects : true}).bad_http_get_request
+            });
+        } else {
+            const result = await database.selectAvailableFlightsFiltered(request.query.departureAirport, request.query.arrivalAirport, request.query.departureDate, request.query.numOfPassengers);
+
+            response.status(200).json({
+                flights: result
+            });
+        }
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/geterrors', (request, response) => {
+    try {
+
+        response.status(200).json({
+            errors: request.t("errors", {returnObjects : true})
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/getflights', (request, response) => {
+    try {
+
+        response.status(200).json({
+            flights: request.t("flights", {returnObjects : true})
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+router.get('/getnavbar', (request, response) => {
+    try {
+
+        response.status(200).json({
+            navbar: request.t("navbar", {returnObjects : true})
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});
+
+/*router.get('/a', (request, response) => {
+    try {
+        
+        
+
+        response.status(200).json({
+            message : 
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: error
+        });
+    }
+});*/
+
+
+router.post('/login', async (request, response) => {
+    try {
+        const { email, password } = request.body
+        if (email && password) {
+            const login = await database.Login(email);
+            if (!login) {
+                return response.status(400).json({
+                    message: 'Rossz email vagy jelszó'
+                });
+            }
+            else {
+                if (await bcrypt.compare(password, login.UserPassword)) {
+                    request.session.user = {
+                        id: login.UserID,
+                        role: login.AdminStatus,
+                        timestamp: Date.now()
+                    }
+                    console.log(request.session.user)
+                    if (request.session.user.role === 1) {
+                        response.status(201).json({
+                            message: 'Sikeres bejelentkezés',
+                            admin: true
+                        });
+                    }
+                    response.status(201).json({
+                        message: 'Sikeres bejelentkezés',
+                        admin: false
+                    });
+                }
+                else {
+                    response.status(400).json({
+                        message: 'Rossz email vagy jelszó'
+                    });
+                }
+            }
+        }
+        else {
+            response.status(400).json({
+                message: 'A felhasználó nem adta meg valamelyik adatot'
+            });
+        }
+    } catch (error) {
+        response.status(500).json({
+            message: 'Ez a végpont nem működik.'
+        });
+    }
+});
+
+router.post('/register', async (request, response) => {
+    try {
+        const { nev, email, jelszo, szuldatum } = request.body
+        if (!nev || !email || !jelszo || !szuldatum) {
+            return response.status(400).json({
+                message: 'A felhasználó nem adta meg valamelyik adatot'
+            });
+        }
+        else {
+            const saltRounds = 10;
+            const hashedPassword = await bcrypt.hash(jelszo, saltRounds);
+            const register = await database.Register(nev, email, hashedPassword, szuldatum, 0, 1);
+            if (!register) {
+                return response.status(400).json({
+                    message: 'Sikertelen regisztráció'
+                });
+            }
+            else {
+                response.status(200).json({
+                    message: 'Sikeres Regisztráció',
+                });
+            }
+        }
+    } catch (error) {
+        response.status(500).json({
+            message: 'Ez a végpont nem működik.'
+        });
+    }
+});
+
+
+router.post('/logout', async (request, response) => {
+    try {
+        if (!request.session) {
+            return response.status(200).json({
+                message: 'Nincs aktív session'
+            });
+        }
+        request.session.destroy(err => {
+            if (err) {
+                console.error('Session destroy hiba:', err);
+                return response.status(500).json({ message: 'Sikertelen kijelentkezés' })
+            }
+            response.clearCookie('connect.sid');
+            return response.status(200).json({
+                message: 'Sikeres Kijelentkezés'
+            })
+        })
+    } catch (error) {
+        console.error('Logout Hiba:', error);
+        return response.status(500).json({ message: 'Szerverhiba a kijelentkezés' });
+    }
+});
+
+router.get('/husegprogram', async (request, response) => {
+    try {
+        if (!LoggedInCheck(request)) {
+            return response.status(220).json({
+                message: "Nem vagy bejelentkezve!"
+            })
+        }
+        else {
+            const user = await database.Husegprogram(request.session.user.id);
+            response.status(200).json({
+                message: "Siker",
+                result: user
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({
+            message: 'Ez a végpont nem működik.'
+        });
+    }
+});
+
+function LoggedInCheck(request) {
+    let vissza = false;
+    if (request.session && request.session.user && request.session.user.id && request.session.user.role !== undefined && request.session.user.timestamp) {
+        if (Date.now() - request.session.user.timestamp > 600000) {
+            request.session.destroy();
+        }
+        else {
+            request.session.user.timestamp = Date.now();
+            vissza = true;
+        }
+    }
+    return vissza;
+}
+
+router.get('/helyfoglalas', async (request, response) => {
+    try {
+        if (!LoggedInCheck(request)) {
+            throw new Error("Nem vagy bejelentkezve!")
+        }
+        else {
+            let id = request.query.id;
+            if (!id) {
+                throw new Error("Nincs járat id")
+            }
+            const helyek = await database.selectAvailableSeatsOnFlight(id, request.session.user.id);
+            response.status(200).json({
+                helyek: helyek
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+router.post('/helyfoglalas', async (request, response) => {
+    try {
+        if (!LoggedInCheck(request)) {
+            throw new Error("Nem vagy bejelentkezve!")
+        }
+        else {
+            const{flightID, rowID, columnID} = request.body;
+            if (!flightID || !rowID || !columnID) {
+                throw new Error("Hiányzó adat.")
+            }
+            const siker = await database.SeatReservation(request.session.user.id, flightID, rowID, columnID);
+            if (!siker) {
+                throw new Error("Hiba az adatbázisban")
+            }
+            response.status(200).json({
+                siker: siker
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+>>>>>>> Stashed changes
 module.exports = router;
--- backend/sql/database.js
+++ backend/sql/database.js
@@ -16,7 +16,168 @@ async function selectall() {
     const [rows] = await pool.execute(query);
     return rows;
 }
+<<<<<<< Updated upstream
 //!Export
 module.exports = {
     selectall
+=======
+
+async function selectAvailableDepartureAirports() {
+    const query = 'SELECT DISTINCT DepartureAirport FROM flights_without_ids WHERE DepartureDateTime > NOW();';
+    const [rows] = await pool.execute(query);
+    return rows;
+}
+
+async function selectAvailableArrivalAirports() {
+    const query = 'SELECT DISTINCT ArrivalAirport FROM flights_without_ids WHERE DepartureDateTime > NOW();';
+    const [rows] = await pool.execute(query);
+    return rows;
+}
+
+async function selectAvailableFlightsBasedOnParameters(departureAirport, arrivalAirport, departureDate) {
+    const query = 'SELECT * FROM available_flights_simplified WHERE DepartureAirport LIKE ? AND ArrivalAirport LIKE ? AND DepartureDate LIKE ?;';
+    const [rows] = await pool.execute(query, [`%${departureAirport}%`, `%${arrivalAirport}%`, `%${departureDate}%`]);
+    return rows;
+}
+
+async function selectAvailableDepartureAirportsFilteredHun(arrivalAirport, departureDate) {
+    const query = 'SELECT DISTINCT DepartureAirport AS "AirportCode", (SELECT Hungarian FROM city INNER JOIN airport ON city.CityID = airport.CityId WHERE airport.AirportCode LIKE DepartureAirport) AS "City", (SELECT Hungarian FROM country INNER JOIN airport ON country.CountryID = airport.CountryId WHERE airport.AirportCode LIKE DepartureAirport) AS "Country" FROM available_flights_simplified WHERE ArrivalAirport LIKE ? AND DepartureDate LIKE ? ORDER BY Country ASC, City ASC;';
+    const [rows] = await pool.execute(query, [`%${arrivalAirport}%`, `%${departureDate}%`]);
+    return rows;
+}
+
+async function selectAvailableArrivalAirportsFilteredHun(departureAirport, departureDate) {
+    const query = 'SELECT DISTINCT ArrivalAirport AS "AirportCode", (SELECT Hungarian FROM city INNER JOIN airport ON city.CityID = airport.CityId WHERE airport.AirportCode LIKE ArrivalAirport) AS "City", (SELECT Hungarian FROM country INNER JOIN airport ON country.CountryID = airport.CountryId WHERE airport.AirportCode LIKE ArrivalAirport) AS "Country" FROM available_flights_simplified WHERE DepartureAirport LIKE ? AND DepartureDate LIKE ? ORDER BY Country ASC, City ASC;';
+    const [rows] = await pool.execute(query, [`%${departureAirport}%`, `%${departureDate}%`]);
+    return rows;
+}
+
+async function selectAvailableDepartureAirportsFilteredEng(arrivalAirport, departureDate) {
+    const query = 'SELECT DISTINCT DepartureAirport AS "AirportCode", (SELECT English FROM city INNER JOIN airport ON city.CityID = airport.CityId WHERE airport.AirportCode LIKE DepartureAirport) AS "City", (SELECT English FROM country INNER JOIN airport ON country.CountryID = airport.CountryId WHERE airport.AirportCode LIKE DepartureAirport) AS "Country" FROM available_flights_simplified WHERE ArrivalAirport LIKE ? AND DepartureDate LIKE ? ORDER BY Country ASC, City ASC;';
+    const [rows] = await pool.execute(query, [`%${arrivalAirport}%`, `%${departureDate}%`]);
+    return rows;
+}
+
+async function selectAvailableArrivalAirportsFilteredEng(departureAirport, departureDate) {
+    const query = 'SELECT DISTINCT ArrivalAirport AS "AirportCode", (SELECT English FROM city INNER JOIN airport ON city.CityID = airport.CityId WHERE airport.AirportCode LIKE ArrivalAirport) AS "City", (SELECT English FROM country INNER JOIN airport ON country.CountryID = airport.CountryId WHERE airport.AirportCode LIKE ArrivalAirport) AS "Country" FROM available_flights_simplified WHERE DepartureAirport LIKE ? AND DepartureDate LIKE ? ORDER BY Country ASC, City ASC;';
+    const [rows] = await pool.execute(query, [`%${departureAirport}%`, `%${departureDate}%`]);
+    return rows;
+}
+
+async function selectAvailableDepartureDatesFiltered(departureAirport, arrivalAirport) {
+    const query = 'SELECT DISTINCT DepartureDate FROM available_flights_simplified WHERE DepartureAirport LIKE ? AND ArrivalAirport LIKE ?;';
+    const [rows] = await pool.execute(query, [`%${departureAirport}%`, `%${arrivalAirport}%`]);
+    return rows;
+}
+
+// minden paraméter kötelező
+async function selectAvailableReturnDates(departureAirport, arrivalAirport, destinationArrivalDate) {
+    const query = 'SELECT DISTINCT DATE(DepartureDate) AS "ReturnDate" FROM available_flights_simplified WHERE DepartureAirport LIKE ? AND ArrivalAirport LIKE ? AND DepartureDate >= ?;';
+    const [rows] = await pool.execute(query, [`${departureAirport}`, `${arrivalAirport}`, `${destinationArrivalDate}`]);
+    return rows;
+}
+
+async function selectAvailableArrivalDatesFiltered(departureAirport, arrivalAirport, departureDate) {
+    const query = 'SELECT DISTINCT DATE(ArrivalDateTime) AS "ArrivalDate" FROM available_flights WHERE DepartureAirport LIKE ? AND ArrivalAirport LIKE ? AND DATE(DepartureDateTime) LIKE ?;';
+    const [rows] = await pool.execute(query, [`%${departureAirport}%`, `%${arrivalAirport}%`, `%${departureDate}%`]);
+    
+    return rows;
+}
+
+
+async function selectSwappableFlightsWithSameDepartureDates(departureDate) {
+    const query = 'SELECT DISTINCT afs1.DepartureAirport FROM available_flights_simplified afs1, available_flights_simplified afs2 WHERE (afs1.DepartureAirport = afs2.ArrivalAirport AND afs1.ArrivalAirport = afs2.DepartureAirport) AND (afs1.DepartureDate = afs2.DepartureDate) AND afs1.DepartureDate LIKE ?;';
+    const [rows] = await pool.execute(query, [`%${departureDate}%`]);
+    
+    return rows;
+}
+
+async function selectSwappableFlights() {
+    const query = 'SELECT DISTINCT afs1.DepartureAirport FROM available_flights_simplified afs1, available_flights_simplified afs2 WHERE (afs1.DepartureAirport = afs2.ArrivalAirport AND afs1.ArrivalAirport = afs2.DepartureAirport);';
+    const [rows] = await pool.execute(query);
+    
+    return rows;
+}
+
+async function selectAvailableFlightsFiltered(departureAirport, arrivalAirport, departureDate, numOfPassengers) {
+    const query = 'SELECT num_of_available_seats_on_available_flights.* FROM num_of_available_seats_on_available_flights WHERE DepartureAirport LIKE ? AND ArrivalAirport LIKE ? AND DepartureDate LIKE ? AND NumOfAvailableSeats >= ?;';
+    const [rows] = await pool.execute(query, [`${departureAirport}`, `${arrivalAirport}`, `${departureDate}`, `${numOfPassengers}`]);
+    
+    return rows;
+}
+
+async function selectAvailableSeatsOnFlight(flightId, userId) {
+    const query = 'SELECT seat.RowID, seat.ColumnID, seat.FareClassID, (flight.BasePrice*fareclass.Multiplier)*((100-(SELECT loyaltystatus.DiscountInPercentage FROM useraccount INNER JOIN loyaltystatus ON useraccount.LoyaltyStatusID = loyaltystatus.LoyaltyStatusID WHERE useraccount.UserID = ?))/100) AS "Price" FROM seat INNER JOIN fareclass ON seat.FareClassID = fareclass.FareClassID INNER JOIN aircraft ON seat.AircraftModelID = aircraft.AircraftModelID INNER JOIN flight ON aircraft.AircraftID = flight.AircraftID LEFT JOIN not_cancelled_reservations ON flight.FlightID = not_cancelled_reservations.FlightID AND seat.RowID = not_cancelled_reservations.RowID AND seat.ColumnID = not_cancelled_reservations.ColumnID WHERE flight.FlightID = ? AND not_cancelled_reservations.FlightID IS NULL;';
+    const [rows] = await pool.execute(query, [userId, flightId]);
+
+    return rows;
+}
+
+
+/*
+async function a() {
+    const query = 'SELECT DepartureAirport, ArrivalAirport, DATE(DepartureDate) AS "aaa" FROM available_flights_simplified';
+    const [rows] = await pool.execute(query);
+    return rows;
+}
+*/
+    
+async function getUserById(id){
+    const query = 'SELECT * FROM UserAccount WHERE UserID = ?';
+    const [rows] = await pool.execute(query, [id]);
+    return rows;
+}
+
+
+//Register
+async function Register(userName, userEmail, hashedPassword, userBirthDate, numberOfFlights, loyaltystatus){
+    const query = 'INSERT INTO UserAccount (UserName, UserEmail, UserPassword, UserBirthDate, NumberOfFlights, LoyaltyStatusID) VALUES (?, ?, ?, ?, ?, ?)';
+    const [result] = await pool.execute(query, [userName, userEmail, hashedPassword, userBirthDate, numberOfFlights, loyaltystatus]);
+    return result.affectedRows>0;
+}
+
+//LOGIN
+async function Login(email){
+    const query = 'SELECT UserID, AdminStatus, UserPassword FROM UserAccount WHERE UserEmail = ?';
+    const [rows] = await pool.execute(query, [email]);
+    return rows[0] || null;
+}
+
+//Hűségprogram
+async function Husegprogram(id){
+    const query = 'SELECT UserName, NumberOfFlights, LoyaltyStatusName From UserAccount INNER JOIN Loyaltystatus ON UserAccount.LoyaltyStatusID = Loyaltystatus.LoyaltyStatusID WHERE UserAccount.UserId = ?';
+    const [rows] = await pool.execute(query, [id]);
+    return rows;
+}
+
+//Helyfoglalas INSERT
+async function SeatReservation(PassengerID, flightID, rowID, columnID){
+    const query = 'INSERT INTO reservation (PassengerID, FlightID, RowID, ColumnID, IsCancelled) VALUES (?, ?, ?, ?, 0)';
+    const [rows] = await pool.execute(query, [PassengerID, flightID, rowID, columnID]);
+    return rows.affectedRows>0;
+}
+
+//!Export
+module.exports = {
+    Login,
+    getUserById,
+    Register,
+    Husegprogram,
+    selectAllAirportsInHungarian,
+    selectAvailableDepartureAirports,
+    selectAvailableArrivalAirports,
+    selectAvailableFlightsBasedOnParameters,
+    selectAvailableDepartureAirportsFilteredHun,
+    selectAvailableArrivalAirportsFilteredHun,
+    selectAvailableDepartureAirportsFilteredEng,
+    selectAvailableArrivalAirportsFilteredEng,
+    selectAvailableDepartureDatesFiltered,
+    selectAvailableReturnDates,
+    selectAvailableArrivalDatesFiltered,
+    selectSwappableFlightsWithSameDepartureDates,
+    selectSwappableFlights,
+    selectAvailableFlightsFiltered,
+    selectAvailableSeatsOnFlight,
+    SeatReservation
+>>>>>>> Stashed changes
 };
